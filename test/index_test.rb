1dc5ec2a924d4f6e70b4293dae47c485009eef5b
Andrew Kane <acekane1@gmail.com>
2017-02-16 21:23:29 -0800

    Removed wrong comment [skip ci]

--- test/index_test.rb
+++ test/index_test.rb
@@ -136,7 +136,6 @@ class IndexTest < Minitest::Test
     store [{name: "Product A", text: large_value}], Region
     assert_search "product", ["Product A"], {}, Region
     assert_search "hello", ["Product A"], {fields: [:name, :text]}, Region
-    # values that exceed ignore_above are not included in _all field :(
     assert_search "hello", ["Product A"], {}, Region
   end
 
